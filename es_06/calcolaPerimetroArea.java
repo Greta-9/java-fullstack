70b8db9a9d72c1fb578d559364a7704cbe9bc975
Greta-9 <gre.difabio@gmail.com>
2023-05-15 11:37:55 +0200

    calcolaPerimetroArea vers1.3

--- es_06/calcolaPerimetroArea.java
+++ es_06/calcolaPerimetroArea.java
@@ -2,6 +2,15 @@
 public class calcolaPerimetroArea { //la classe deve corrispondere al nome del file
     public static void main(String[] args) { 
 
-        double lato; // questo sarà il parametro che utilizzerò per i calcoli
+        int lato; // questo sarà il parametro che utilizzerò per i calcoli
+        int area;
+        int perimetro;
+        lato = 5; //assegno il valore 5 alla variabile lato
+        area = lato * lato;
+        perimetro = lato * 4;
+        System.out.println(lato);
+        System.out.println(area);
+        System.out.println(perimetro);
+
     }
 }
