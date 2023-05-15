30a9c0a82918ae8f422cd5a311962b57a867b089
Greta-9 <gre.difabio@gmail.com>
2023-05-15 12:02:02 +0200

    calcolaPerimetroArea vers1.6

--- es_06/calcolaPerimetroArea.java
+++ es_06/calcolaPerimetroArea.java
@@ -4,21 +4,21 @@
  * + verifica dubbi (es: differenza println e print)
  */
 
-import java.util.Scanner; //libreria che serve per utilizzare i metodi di scanner
-public class calcolaPerimetroArea { //la classe deve corrispondere al nome del file .java
+import java.util.Scanner; 
+public class calcolaPerimetroArea { 
     public static void main(String[] args) { 
         Scanner input = new Scanner(System.in);
         System.out.print("Inserisci la lunghezza del lato: ");
-        int lato; // questo sarà il parametro che utilizzerò per i calcoli
-        lato = input.nextInt(); // legge il prossimo intero dall'input
+        int lato; 
+        lato = input.nextInt(); 
         int area;
         int perimetro;
         area = lato * lato;
         perimetro = lato * 4;
-        System.out.println("Il lato inserito: " + lato);
+        System.out.println("Il lato inserito: " + lato); // concateno testo e variabile
         System.out.println("L'area e': " + area);
         System.out.println("Il perimetro e': " + perimetro);
-        input.close(); // la chiusura è opzionale, ma se non viene fatta l'input resta evidenziato in giallo
+        input.close(); 
 
     }
 }
