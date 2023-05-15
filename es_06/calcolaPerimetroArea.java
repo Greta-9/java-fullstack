2f2916c45ff0acd13ba8cf89304ece12bc5b1b1c
Greta-9 <gre.difabio@gmail.com>
2023-05-15 10:55:19 +0200

    calcolaPerimetroArea cambio var vers1.2

--- es_06/calcolaPerimetroArea.java
+++ es_06/calcolaPerimetroArea.java
@@ -1,7 +1,7 @@
 
 public class calcolaPerimetroArea { //la classe deve corrispondere al nome del file
     public static void main(String[] args) { 
-        //le istruzioni vanno scritte all'interno delle graffe del metodo main
-        int lato; // questo sarà il parametro che utilizzerò per i calcoli
+
+        double lato; // questo sarà il parametro che utilizzerò per i calcoli
     }
 }
