11de1bdaf10cc90248d6d567661af38615f17f8e
Greta-9 <gre.difabio@gmail.com>
2023-05-15 12:38:58 +0200

    calcolaPerimetroArea agg. nome vers2.0

--- es_06/calcolaPerimetroArea.java
+++ es_06/calcolaPerimetroArea.java
@@ -8,15 +8,17 @@ import java.util.Scanner;
 public class calcolaPerimetroArea { 
     public static void main(String[] args) { 
         Scanner input = new Scanner(System.in);
+        System.out.print("Inserisci il tuo nome: ");
+        String nome = input.nextLine();
         System.out.print("Inserisci la lunghezza del lato: ");
         int lato; 
         lato = input.nextInt(); 
         if (lato > 40 || lato < 1) { 
              if (lato > 40) {
-            System.out.println("Il numero inserito e' troppo grande"); // se l'input>40
+            System.out.println(nome + " il numero che hai inserito e' troppo grande!"); // se l'input>40
             }
              if (lato < 1) {
-            System.out.println("Il numero inserito e' troppo piccolo"); // se l'input<1
+            System.out.println(nome + " il numero che hai inserito e' troppo piccolo!"); // se l'input<1
             }
         }
         else { //se l'input è <= a 40
@@ -24,7 +26,7 @@ public class calcolaPerimetroArea {
         int perimetro;
         area = lato * lato;
         perimetro = lato * 4;
-        System.out.println("Il lato inserito: " + lato); 
+        System.out.println(nome + " il lato che hai inserito e': " + lato); 
         System.out.println("L'area e': " + area);
         System.out.println("Il perimetro e': " + perimetro); 
         }
